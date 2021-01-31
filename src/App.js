c3512f4a5af0b9900bffced5c442d3cc85870a11
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-01-31 17:13:16 +0700

    create context

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./routes/Home";
 import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 import UpdatePage from "./routes/UpdatePage";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
 
 const App = () => {
   return (
-    <div className="container">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/restaurants/:id/update" component={UpdatePage} />
-          <Route exact path="/restaurants/:id" component={RestaurantDetailPage} />
-        </Switch>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              component={UpdatePage}
+            />
+            <Route
+              exact
+              path="/restaurants/:id"
+              component={RestaurantDetailPage}
+            />
+          </Switch>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
 };
 
