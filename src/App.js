3d88f48db61ef6b1d23f1218b225681243dc87bd
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-01-31 07:38:18 +0700

    create route & instal react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./routes/Home";
+import RestaurantdetailPage from "./routes/RestaurantdetailPage";
+import UpdatePage from "./routes/UpdatePage";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+          <Route exact path="/restaurants/:id" component={RestaurantdetailPage} />
+        </Switch>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/routes/RestaurantdetailPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RestaurantdetailPage = () => {
+    return (
+        <div>
+            RestaurantDetail Page
+        </div>
+    )
+}
+
+export default RestaurantdetailPage
